6c68df57e9cf64b86015e9c836b0aba7991081cb
Pranav2907 <71582082+Pranav2907@users.noreply.github.com>
2020-11-24 10:42:07 +0530

    VSG

---
+++ plan.md
@@ -0,0 +1,10 @@
+# Plan of action
+
+-intialise NodeJS Project
+-Intialise our First View
+-Create a Room id
+-Add the ability to view our own Video
+-Add ability to allow others to stream their video
+-Add styling
+-Add messages
+-Add stop video button 
---
+++ public/notes.js
@@ -0,0 +1,100 @@
+function myFunction(){
+    var input = document.getElementById("Year");
+    var a = input.value.toLowerCase();
+    var input1 = document.getElementById("Branch");
+    var b = input1.value.toLowerCase();
+    console.log(a);
+    console.log(b);
+    if (a == "first")
+    {
+        if (b == "it")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1cpUhkzbt-DPuINj9FLJxaHy5jA39euM4");
+        }
+        else if (b == "computer")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1vAqVagIrwpieBc9Jr31KNmMnNx0SPfzX");
+        }
+        else if (b == "mechanical")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1Fm76UtrqVwRYY299I_Vrc0pcqacLl0oK");
+        }
+        else if (b == "civil")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1SpboJkUj9cT_cGv7ypEcy6Kgl4LsymON");
+        }
+        else if(b == "ece")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1eCmA9Kfh3mofKC-hvRqna-b8ZYZHB-Gr");
+        }
+    }
+    else if(a == "second")
+    {
+        if (b == "it")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1-cAPTyGqyANNEMk1_iRH6AX6r2ypMsEX");
+        }
+        else if (b == "computer")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1vy7kSwg6Y0r6qbFVz8_d7jZRBnkB0OyD");
+        }
+        else if (b == "mechanical")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1XPBUi7nloKqTRO3593wkPLGXNjRpnG63");
+        }
+        else if (b == "civil")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1aGZusRym6BSWr_6IjXDWNJqn3hK-7tOl");
+        }
+        else if(b == "ece")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/110IZFc1ZVmrxafri8mJ9A3mvEieutTG_");
+        }
+    }
+    else if(a=="third")
+    {
+        if (b == "it")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1MybAeuQ4U52LBgQLj6mfBs4VP-5A3xlO");
+        }
+        else if (b == "computer")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1340YNeQkFVQPt2FK7aplaBtE3CNzuuc_");
+        }
+        else if (b == "mechanical")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1_jIxSi1uePlUp717QtKIXI6cFCTf-edC");
+        }
+        else if (b == "civil")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1-E_ht7-hchrTKvUMWx2UkdfOl2OtlegB");
+        }
+        else if(b == "ece")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1AWmQaKRb-E1CHDcg8nDegOJLHJfhvg9t");
+        }
+    }
+    else if(a=="last")
+    {
+        if (b == "it")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1VbvA12M9FEVUBG-iOQuA0BastT5W-C7e");
+        }
+        else if (b == "computer")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/13uzNcq5HUOVQy1mTEq2peaf58Q4OgXpG");
+        }
+        else if (b == "mechanical")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/19WN1yQS2JuGTgIV1S5t1NPLksyTsDDgK");
+        }
+        else if (b == "civil")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1Q27y3bEna6s3JCOWz_LJ1w0oeMGqqWQo");
+        }
+        else if(b == "ece")
+        {
+            location.replace("https://drive.google.com/drive/u/1/folders/1lrTslmj6y98YbjaTEt3MJlTgdMBIlIf2");
+        }
+    }
+}
