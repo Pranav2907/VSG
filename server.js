abced46ece50b1c33e989b9a3fb8d284066b8adf
Pranav2907 <71582082+Pranav2907@users.noreply.github.com>
2021-01-05 09:59:41 +0530

    VSG

--- server.js
+++ server.js
@@ -2,22 +2,16 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server)
-const { v4: uuidv4 } = require('uuid');
+
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
     debug: true
 });
-
-app.set('view engine','ejs');
-
-
-app.get('/', (req, res) => {
-    res.render('index');
-})
-
-
-app.use(express.static('public'));
+const { v4: uuidv4 } = require('uuid');
 app.use('/peerjs',peerServer);
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`);
 })
@@ -32,7 +26,8 @@ io.on('connection', socket => {
        socket.to(roomId).broadcast.emit('user-connected',userId );
        socket.on('message', message => {
            io.to(roomId).emit('createMessage', message)
-       })
+       });
+       
 
     })
 })
